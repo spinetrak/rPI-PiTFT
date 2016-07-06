510b44cc0ca5ec068da00e8ef36c532d06beae65
spinetrak <spinetrak@spinetrak.net>
2016-07-06 22:10:38 +0200

    - test num pad

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,14 @@
 package sample;
 
-import javafx.animation.AnimationTimer;
-import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
-import javafx.scene.chart.AreaChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart.Series;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * A chart that fills in the area between a line of data points and the axes.
@@ -26,93 +21,80 @@ import java.util.logging.Logger;
  * @related charts/scatter/ScatterChart
  */
 public class Main extends Application {
-    private static final int MAX_DATA_POINTS = 50;
 
-    private Series series;
-    private int xSeriesData = 0;
-    private ConcurrentLinkedQueue<Number> dataQ = new ConcurrentLinkedQueue<Number>();
-    private ExecutorService executor;
-    private AddToQueue addToQueue;
-    private Timeline timeline2;
-    private NumberAxis xAxis;
 
-    private void init(Stage primaryStage) {
-        xAxis = new NumberAxis(0,MAX_DATA_POINTS,MAX_DATA_POINTS/10);
-        xAxis.setForceZeroInRange(false);
-        xAxis.setAutoRanging(false);
-
-        NumberAxis yAxis = new NumberAxis();
-        yAxis.setAutoRanging(true);
-
-        //-- Chart
-        final AreaChart<Number, Number> sc = new AreaChart<Number, Number>(xAxis, yAxis) {
-            // Override to remove symbols on each data point
-            @Override protected void dataItemAdded(Series<Number, Number> series, int itemIndex, Data<Number, Number> item) {}
-        };
-        sc.setAnimated(false);
-        sc.setId("liveAreaChart");
-        sc.setTitle("Animated Area Chart");
-
-        //-- Chart Series
-        series = new AreaChart.Series<Number, Number>();
-        series.setName("Area Chart Series");
-        sc.getData().add(series);
-
-        primaryStage.setScene(new Scene(sc));
-    }
 
     @Override public void start(Stage primaryStage) throws Exception {
-        init(primaryStage);
-        primaryStage.show();
+        int width = 240;
+        int height = 320;
 
-        //-- Prepare Executor Services
-        executor = Executors.newCachedThreadPool();
-        addToQueue = new AddToQueue();
-        executor.execute(addToQueue);
-        //-- Prepare Timeline
-        prepareTimeline();
+        primaryStage.setTitle("DoorNFX");
+
+        Pane root = new Pane();
+
+        root.setPrefWidth(width);
+        root.setPrefHeight(height);
+
+        Label statusBox = new Label();
+        statusBox.setPrefWidth(210);
+        statusBox.setPrefHeight(35);
+        statusBox.setAlignment(Pos.CENTER);
+        statusBox.setTextFill(Color.web("#bbbbff"));
+        statusBox.setLayoutY(5);
+        statusBox.setLayoutX(15);
+        statusBox.setText("Tag in");
+        root.getChildren().add(statusBox);
+
+
+
+        root.getChildren().add(createAndRegisterNumButton("<", 250, 15));
+        root.getChildren().add(createAndRegisterNumButton("0", 250, 90));
+        root.getChildren().add(createAndRegisterNumButton("!", 250, 165));
+
+        root.getChildren().add(createAndRegisterNumButton("7", 185, 15));
+        root.getChildren().add(createAndRegisterNumButton("8", 185, 90));
+        root.getChildren().add(createAndRegisterNumButton("9", 185, 165));
+
+        root.getChildren().add(createAndRegisterNumButton("4", 120, 15));
+        root.getChildren().add(createAndRegisterNumButton("5", 120, 90));
+        root.getChildren().add(createAndRegisterNumButton("6", 120, 165));
+
+        root.getChildren().add(createAndRegisterNumButton("1", 55, 15));
+        root.getChildren().add(createAndRegisterNumButton("2", 55, 90));
+        root.getChildren().add(createAndRegisterNumButton("3", 55, 165));
+
+
+        Scene mainScene = new Scene(root, width, height);
+        mainScene.setCursor(Cursor.NONE);
+
+
+        primaryStage.setScene(mainScene);
+        primaryStage.setFullScreen(true);
+        primaryStage.show();
+    }
+
+    private Button createAndRegisterNumButton(String caption, int y, int x) {
+        Button roundButton = new Button();
+
+        roundButton.setStyle(
+          "-fx-background-radius: 5em; " +
+            "-fx-min-width: 60px; " +
+            "-fx-min-height: 60px; " +
+            "-fx-max-width: 60px; " +
+            "-fx-max-height: 60px; " +
+            "-fx-font-size: 18pt; -fx-font-weight:bold; "
+        );
+
+        roundButton.setText(caption);
+        roundButton.setLayoutX(x);
+        roundButton.setLayoutY(y);
+        roundButton.setFocusTraversable(false);
+
+        return roundButton;
     }
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    private class AddToQueue implements Runnable {
-        public void run() {
-            try {
-                // add a item of random data to queue
-                dataQ.add(Math.random());
-                Thread.sleep(500);
-                executor.execute(this);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
-
-    //-- Timeline gets called in the JavaFX Main thread
-    private void prepareTimeline() {
-        // Every frame to take any data from queue and add to chart
-        new AnimationTimer() {
-            @Override public void handle(long now) {
-                addDataToSeries();
-            }
-        }.start();
-    }
-
-    private void addDataToSeries() {
-        //for (int i = 0; i < 20; i++) { //-- add 20 numbers to the plot+
-            if (!dataQ.isEmpty())
-            {
-                series.getData().add(new AreaChart.Data(xSeriesData++, dataQ.remove()));
-            }
-        //}
-        // remove points to keep us at no more than MAX_DATA_POINTS
-        if (series.getData().size() > MAX_DATA_POINTS) {
-            series.getData().remove(0, series.getData().size() - MAX_DATA_POINTS);
-        }
-        // update 
-        xAxis.setLowerBound(xSeriesData-MAX_DATA_POINTS);
-        xAxis.setUpperBound(xSeriesData-1);
-    }
 }
