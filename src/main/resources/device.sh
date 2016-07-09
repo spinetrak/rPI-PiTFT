2e17364c9228da470fa868c67bbfd12d81693c42
spinetrak <spinetrak@spinetrak.net>
2016-07-10 00:45:28 +0200

    - add device data

--- src/main/resources/device.sh
+++ src/main/resources/device.sh
@@ -4,4 +4,4 @@ cpu=$(/bin/cat <(grep 'cpu ' /proc/stat) | awk -v RS="" '{print ($13-$2+$15-$4)*
 disk=$(/bin/df -lh | awk '{if ($6 == "/") { print $5 }}' | head -1 | cut -d'%' -f1)
 temperature=$(/opt/vc/bin/vcgencmd measure_temp | awk -F "=" '{print $2}' | awk -F "'" '{print $1}')
 
-echo "${cpu}/${disk}/${temperature}V"
+echo "${cpu}/${disk}/${temperature}"
