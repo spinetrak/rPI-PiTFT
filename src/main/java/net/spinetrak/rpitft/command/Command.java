2b3a4b53a2c9e51415bffdbb85f2d05147a9563d
spinetrak <spinetrak@spinetrak.net>
2016-07-09 23:27:52 +0200

    - add device data

--- src/main/java/net/spinetrak/rpitft/command/Command.java
+++ src/main/java/net/spinetrak/rpitft/command/Command.java
@@ -5,17 +5,26 @@ import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.PumpStreamHandler;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class Command
 {
-  public static String execute(final String command_) throws Exception
+  public static String execute(final String command_)
   {
     final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
     final CommandLine commandline = CommandLine.parse(command_);
     final DefaultExecutor exec = new DefaultExecutor();
     final PumpStreamHandler streamHandler = new PumpStreamHandler(outputStream);
     exec.setStreamHandler(streamHandler);
-    exec.execute(commandline);
+    try
+    {
+      exec.execute(commandline);
+    }
+    catch (final IOException ex_)
+    {
+      ex_.printStackTrace();
+      System.err.println("Command failed: [" + command_ + "]");
+    }
     return (outputStream.toString());
   }
 }
