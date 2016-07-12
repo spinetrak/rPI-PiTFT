c6449b32639a59b764b99fde08369ecb492734b8
spinetrak <spinetrak@spinetrak.net>
2016-07-13 01:22:35 +0200

    - add GPS data to chart

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
+cd /home/pi/java/rPI-PiTFT
+sudo mvn exec:java -Djava.ext.dirs=/usr/lib/jvm/openjfx-8u60-sdk-overlay-linux-armv6hf/jre/lib/ext/ -Djavafx.platform=monocle -Dmonocle.screen.fb=/dev/fb1  -Dexec.mainClass="net.spinetrak.rpitft.ui.Main" > /var/tmp/rpitft.log 2>&1
 
-cd /home/pi/java/rPI-PiTFT & fbcp & sudo mvn package & sudo mvn exec:java -Djava.ext.dirs=/usr/lib/jvm/openjfx-8u60-sdk-overlay-linux-armv6hf/jre/lib/ext/ -Djavafx.platform=monocle -Dmonocle.screen.fb=/dev/fb1  -Dexec.mainClass="net.spinetrak.rpitft.ui.Main" > /var/tmp/rpitft.log 2>&1
-killall fbcp
 
