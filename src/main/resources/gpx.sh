255b0f37907a435331776a277cf8cd919b1a9a28
spinetrak <spinetrak@spinetrak.net>
2016-07-20 08:58:37 +0200

    - fix GPS time display

--- src/main/resources/gpx.sh
+++ src/main/resources/gpx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-now=$(date +%F_%H:%M:%S)
+now=$(date +%F_%H-%M-%S)
 /usr/bin/gpsbabel -D 3 -i nmea -f /home/pi/tracks/nmea.txt -x simplify,count=1500 -o gpx,gpxver=1.1 -F /home/pi/tracks/gpx/$now.gpx
