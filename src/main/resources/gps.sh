463c19caea11ca877e5b299d258140a085ac7032
spinetrak <spinetrak@spinetrak.net>
2016-08-03 21:30:00 +0200

    - fix gps.sh script

--- src/main/resources/gps.sh
+++ src/main/resources/gps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GGA=$(tail -n 25 /home/pi/tracks/nmea.txt | grep -a GGA)
+GGA=$(tail -n 100 /home/pi/tracks/nmea.txt | grep -a GGA | tail -n 1)
 time=$(echo ${GGA} | awk -F',' '{ print $2 }' | awk -F'.' '{ print $1 }')
 latitude=$(echo ${GGA} | awk -F',' '{ print $3 }')
 northsouth=$(echo ${GGA} | awk -F',' '{ print $4 }')
