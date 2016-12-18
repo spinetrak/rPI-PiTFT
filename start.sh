bc2fbb0b08b2a0d2e7bc25928752ccecc14ebda5
spinetrak <spinetrak@spinetrak.net>
2016-12-18 20:54:59 +0100

    - log to /var/log

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ then
     sudo ln -s /dev/ttyACM0 /dev/ttyS80
 fi
 
-mvn install
-sudo java -cp target/rPI-PiTFT-1.0-SNAPSHOT-jar-with-dependencies.jar -Djava.library.path=/usr/lib/jni -Djava.ext.dirs=/usr/lib/jvm/openjfx-8u60-sdk-overlay-linux-armv6hf/jre/lib/ext/ -Djavafx.platform=monocle -Dmonocle.screen.fb=/dev/fb1  net.spinetrak.rpitft.ui.Main  > /var/log/rpitft.log 2>&1
+mvn install > /var/log/rpitft.log 2>&1
+sudo java -cp target/rPI-PiTFT-1.0-SNAPSHOT-jar-with-dependencies.jar -Djava.library.path=/usr/lib/jni -Djava.ext.dirs=/usr/lib/jvm/openjfx-8u60-sdk-overlay-linux-armv6hf/jre/lib/ext/ -Djavafx.platform=monocle -Dmonocle.screen.fb=/dev/fb1  net.spinetrak.rpitft.ui.Main  >> /var/log/rpitft.log 2>&1
 
 
