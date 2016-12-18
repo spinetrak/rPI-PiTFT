e60057043f15c6ce4885cfb0693a4929bcb66548
spinetrak <spinetrak@spinetrak.net>
2016-12-18 20:47:08 +0100

    - use single fat JAR at runtime

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ then
     sudo ln -s /dev/ttyACM0 /dev/ttyS80
 fi
 
-sudo mvn exec:java -Djava.library.path=/usr/lib/jni -Djava.ext.dirs=/usr/lib/jvm/openjfx-8u60-sdk-overlay-linux-armv6hf/jre/lib/ext/ -Djavafx.platform=monocle -Dmonocle.screen.fb=/dev/fb1  -Dexec.mainClass="net.spinetrak.rpitft.ui.Main" > /var/tmp/rpitft.log 2>&1
+mvn install
+sudo sudo java -cp target/rPI-PiTFT-1.0-SNAPSHOT-jar-with-dependencies.jar -Djava.library.path=/usr/lib/jni -Djava.ext.dirs=/usr/lib/jvm/openjfx-8u60-sdk-overlay-linux-armv6hf/jre/lib/ext/ -Djavafx.platform=monocle -Dmonocle.screen.fb=/dev/fb1  net.spinetrak.rpitft.ui.Main  > /var/tmp/rpitft.log 2>&1
 
 
